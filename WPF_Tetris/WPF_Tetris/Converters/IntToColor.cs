a79d9463ad1442326dc10195749591bb7652ce7e
하로스튜디오 <ds3evx@hotmail.com>
2020-03-10 05:51:16 +0900

    wpf 첫 커밋

---
+++ WPF_Tetris/WPF_Tetris/Converters/IntToColor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using WPF_Tetris.Models;
+
+namespace WPF_Tetris.Converters
+{
+    public class IntToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int type = (int)value;
+
+            switch (type)
+            {
+                case Constants.Block_background:
+                    if (parameter != null)
+                        return new SolidColorBrush(Colors.Transparent);
+                    return new SolidColorBrush(Colors.Black);
+                case Constants.Block_wall:
+                    return new SolidColorBrush(Colors.Ivory);
+                case Constants.Block_1:
+                    return new SolidColorBrush(Colors.Green);
+                case Constants.Block_2:
+                    return new SolidColorBrush(Colors.Blue);
+                case Constants.Block_3:
+                    return new SolidColorBrush(Colors.Red);
+                case Constants.Block_4:
+                    return new SolidColorBrush(Colors.Yellow);
+                case Constants.Block_5:
+                    return new SolidColorBrush(Colors.Magenta);
+                case Constants.Block_6:
+                    return new SolidColorBrush(Colors.Lime);
+            }
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF_Tetris/WPF_Tetris/Views/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Tetris.ViewModels;
+
+namespace WPF_Tetris.Views
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)DataContext).EnterGame();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MainWindowViewModel mv = (MainWindowViewModel)DataContext;
+            if (!mv.Is_gaming) return;
+            switch (e.Key)
+            {
+                case Key.Left:
+                    mv.BlockMoveLeft();
+                    break;
+                case Key.Right:
+                    mv.BlockMoveRight();
+                    break;
+                case Key.Space:
+                    mv.Block_drop();
+                    break;
+                case Key.Down:
+                    mv.Block_down();
+                    break;
+                case Key.Up:
+                    mv.BlockRotate();
+                    break;
+            }
+        }
+    }
+}
